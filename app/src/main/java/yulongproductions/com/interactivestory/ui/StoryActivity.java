a676ccadf8b6b4f43c65ad26474b35fc17f1d930
Yulong Tan <swifties@uw.edu>
2015-07-20 23:44:02 -0700

    linked story with layouts

--- app/src/main/java/yulongproductions/com/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/yulongproductions/com/interactivestory/ui/StoryActivity.java
@@ -1,15 +1,30 @@
 package yulongproductions.com.interactivestory.ui;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import yulongproductions.com.interactivestory.R;
+import yulongproductions.com.interactivestory.model.Page;
+import yulongproductions.com.interactivestory.model.Story;
 
 
-public class StoryActivity extends ActionBarActivity {
+public class StoryActivity extends Activity {
 
+    // Fields
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+
+    // Class constants
     public static final String TAG = StoryActivity.class.getSimpleName();
 
     @Override
@@ -18,10 +33,31 @@ public class StoryActivity extends ActionBarActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
-        if (name == null) {
-            name = "Arkham Knight";
+        mName = intent.getStringExtra(getString(R.string.key_name));
+        if (mName == null) {
+            mName = "The Arkham Knight";
         }
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+
+        loadPage();
+    }
+
+    private void loadPage() {
+        Page page = mStory.getPage(0);
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+
+        mImageView.setImageDrawable(drawable);
+        String pageText = page.getText();
+        // Add the name if placeholder included. Won't add if no placeholder
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
     }
 }
